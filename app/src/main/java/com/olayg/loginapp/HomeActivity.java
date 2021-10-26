84a602755bd08caa4b72633c4a5bf41a74a1cda1
tiamabderezai <pass>
2021-10-26 19:53:14 -0400

    login and homepage complete

---
+++ app/src/main/java/com/olayg/loginapp/HomeActivity.java
@@ -0,0 +1,28 @@
+package com.olayg.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.olayg.loginapp.databinding.ActivityHomeBinding;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.tvWelcome.setText(getIntent().getStringExtra("key"));
+    }
+}
