9253d929aebdb5aa1052f85c1332dd5da9b47779
Jimmy McBride <jimmymcbride@jimmys-air.lan>
2021-10-26 18:47:12 -0400

    more touch ups

--- app/src/main/java/com/olayg/loginapp/LoginActivity.java
+++ app/src/main/java/com/olayg/loginapp/LoginActivity.java
@@ -3,13 +3,8 @@ package com.olayg.loginapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-//import android.inputmethodservice.KeyboardView;
 import android.os.Bundle;
-//import android.os.Handler;
-//import android.renderscript.ScriptGroup;
-//import android.util.Log;
 import android.view.View;
-//import android.widget.Button;
 
 import com.olayg.loginapp.databinding.ActivityLoginBinding;
 
