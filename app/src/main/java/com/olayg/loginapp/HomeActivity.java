bf883089fccbe44d2cd44d07164151e55cfa332b
Jimmy McBride <jimmymcbride@Jimmys-MacBook-Air.local>
2021-10-26 16:22:31 -0400

    finished assignment

--- app/src/main/java/com/olayg/loginapp/HomeActivity.java
+++ app/src/main/java/com/olayg/loginapp/HomeActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.olayg.loginapp.databinding.ActivityHomeBinding;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ActivityHomeBinding binding = ActivityHomeBinding.inflate(getLayoutInflater());
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(binding.getRoot());
+        binding.tvWelcome.setText("Hello, FireNinja");
     }
 }
