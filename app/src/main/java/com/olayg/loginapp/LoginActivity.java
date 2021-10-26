631cb1e974ae7d2c6c5fa2fff949158bf571c0b0
Jimmy McBride <jimmymcbride@jimmys-air.lan>
2021-10-26 18:45:46 -0400

    touch ups

--- app/src/main/java/com/olayg/loginapp/LoginActivity.java
+++ app/src/main/java/com/olayg/loginapp/LoginActivity.java
@@ -3,15 +3,17 @@ package com.olayg.loginapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.inputmethodservice.KeyboardView;
+//import android.inputmethodservice.KeyboardView;
 import android.os.Bundle;
 //import android.os.Handler;
-import android.renderscript.ScriptGroup;
-import android.util.Log;
+//import android.renderscript.ScriptGroup;
+//import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+//import android.widget.Button;
 
 import com.olayg.loginapp.databinding.ActivityLoginBinding;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -24,7 +26,7 @@ public class LoginActivity extends AppCompatActivity {
 
         binding.btnLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String username = binding.usernameContainer.getEditText().getText().toString();
+                String username = Objects.requireNonNull(binding.usernameContainer.getEditText()).getText().toString();
                 // Code here executes on main thread after user presses button
                 Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                 intent.putExtra("extraString", username);
@@ -33,12 +35,4 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-//    @Override
-//    public View onCreateInputView() {
-//        KeyboardView inputView = (KeyboardView) getLayoutInflater().inflate(R.layout.activity_login, null);
-//
-//        inputView.setOnKeyboardActionListener((KeyboardView.OnKeyboardActionListener) this);
-//
-//        return inputView;
-//    }
 }
