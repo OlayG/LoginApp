0c08677dd44f3289c90c0b9a99fa0967030454bc
Jimmy McBride <jimmymcbride@Jimmys-MacBook-Air.local>
2021-10-27 11:49:30 -0400

    fixed up some things and cleaned up some code

--- app/src/main/java/com/olayg/loginapp/LoginActivity.java
+++ app/src/main/java/com/olayg/loginapp/LoginActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import com.olayg.loginapp.databinding.ActivityLoginBinding;
 
@@ -14,19 +13,19 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
 
-        super.onCreate(savedInstanceState);
         setContentView(binding.getRoot());
 
-        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String username = Objects.requireNonNull(binding.usernameContainer.getEditText()).getText().toString();
-                // Code here executes on main thread after user presses button
-                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                intent.putExtra("extraString", username);
-                startActivity(intent);
-            }
+        binding.btnLogin.setOnClickListener(v -> {
+            String username = Objects.requireNonNull(binding.usernameContainer.getEditText())
+                    .getText()
+                    .toString();
+            // Code here executes on main thread after user presses button
+            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+            intent.putExtra("extraString", username);
+            startActivity(intent);
         });
     }
 
