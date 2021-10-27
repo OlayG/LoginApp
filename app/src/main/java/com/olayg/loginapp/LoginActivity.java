d72e917f657db7d653412ea615176d4d73e93b75
matthewogtong <ogtongm@gmail.com>
2021-10-27 12:15:47 -0400

    Optimize LoginActivity

--- app/src/main/java/com/olayg/loginapp/LoginActivity.java
+++ app/src/main/java/com/olayg/loginapp/LoginActivity.java
@@ -5,11 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.button.MaterialButton;
 import com.olayg.loginapp.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
-
-    String username, password;
 
     private ActivityLoginBinding binding;
 
@@ -17,14 +16,39 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        View view = binding.getRoot();
+        setContentView(view);
 
-        binding.btnLogin.setOnClickListener((View v) -> {
-            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-            //Adding data to the Intent for next component
-            intent.putExtra("usernameData", binding.usernameContainer.getEditText().getText().toString());
-            startActivity(intent);
+        MaterialButton button = binding.btnLogin;
+
+        // Lambda way (JAVA 8+) removes excess boilerplate, all you need is parameter and scope
+        button.setOnClickListener(v -> {
+
         });
+
+        String name = binding.usernameContainer.getEditText().getText().toString();
+        String password = binding.passwordContainer.getEditText().getText().toString();
+
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent is a way to let the OS know you want to navigate between two components
+                // Can be used to pass data, we are using it explicitly
+                                            // Start point , End point
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                intent.putExtra("username", name);
+                intent.putExtra("password", password);
+                startActivity(intent);
+
+            }
+        });
+        // JAVA 7
+//        button.setOnClickListener((View v) -> {
+//            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+//            //Adding data to the Intent for next component
+//            intent.putExtra("usernameData", binding.usernameContainer.getEditText().getText().toString());
+//            startActivity(intent);
+//        });
 
     }
 }
