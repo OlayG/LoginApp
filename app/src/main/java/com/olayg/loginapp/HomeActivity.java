d9dee815466dc2827d70017af0a37d7a63af0a2a
matthewogtong <ogtongm@gmail.com>
2021-10-27 10:49:24 -0400

    Add comments to code

--- app/src/main/java/com/olayg/loginapp/HomeActivity.java
+++ app/src/main/java/com/olayg/loginapp/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.olayg.loginapp;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,13 +10,17 @@ import com.olayg.loginapp.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
 
+    // ActivityHomeBinding is always referenced to the xml file it's related to (most likely in your resource -> layout package
     private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // .inflate goes converts xml to the java class we'll be using
+        // getLayoutInflater comes from parent/base/super class of ActivityHomeBinding
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        View view = binding.getRoot();
+        setContentView(view);
         String usernameValue = getIntent().getStringExtra("usernameData");
         binding.tvWelcome.setText("Hello, " + usernameValue);
     }
