b78ed9e33b6866c2209418d3851b5b1a1d32f1ac
matthewogtong <ogtongm@gmail.com>
2021-10-26 22:23:47 -0400

    Render getIntent and display usernameData

--- app/src/main/java/com/olayg/loginapp/HomeActivity.java
+++ app/src/main/java/com/olayg/loginapp/HomeActivity.java
@@ -5,10 +5,18 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.olayg.loginapp.databinding.ActivityHomeBinding;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        String usernameValue = getIntent().getStringExtra("usernameData");
+        binding.tvWelcome.setText("Hello, " + usernameValue);
     }
 }
